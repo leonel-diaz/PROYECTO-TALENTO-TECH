7d10887dfadd73ca27ae01c570f560cdffdd0ee5
leonel-diaz <nemesis.94@hotmail.com>
2024-08-11 09:01:18 -0500

    agregando varios archivos

--- app.js
+++ app.js
@@ -5,20 +5,13 @@ const port = 3000;
 // milddeware para contenido estatico
 app.use(express.static(__dirname +'/public'));
 
+ 
+
 // motor de plantillas
 app.set('view engine', 'ejs');
 app.set('views',__dirname +'/views');
 
-
-app.get ('/',function (rep,res) {
-    // res.send("hello word");
-    res.render('index',{titulo:"titulo dinamico con un motor de plantilla"});
-});
-
-// ejemplo de enrutamiento
-app.get('/servicios',(rep,res)=>{
-  res.render('servicios',{titulo:"nuestros servicios dinamicos"});
-})
+app.use('/',require('./router/rutasweb'))
 
 // direccionar lista 404 
 app.use((res,req,next)=>{
---
+++ router/rutasweb.js
@@ -0,0 +1,16 @@
+var express = require("express");
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.render('index',{titulo:'bienvenido a node js '})
+  })
+  
+router.get('/',(rep,res)=>{
+    res.render('servicios',{titulo:"nuestros servicios dinamicos"});
+  });
+
+  module.exports= router;
+  
+  
+  
