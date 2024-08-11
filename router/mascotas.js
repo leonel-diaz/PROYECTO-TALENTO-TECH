69e80e15b7406aab4912a45f14526ed0e3fb2ea0
leonel-diaz <nemesis.94@hotmail.com>
2024-08-11 14:15:01 -0500

    vista de mascotas dinamica

--- router/mascotas.js
+++ router/mascotas.js
@@ -2,7 +2,11 @@ var express = require("express");
 var router = express();
 
 router.get('/', (req, res) => {
-    res.render('mascotas',{titulo:'bienvenido de mascotas'})
+    res.render('mascotas',{arrayMascotas:[
+      {id:"001",nombre:"kiara",descripcion:"bulterrier"},
+      {id:"002",nombre:"rocgh",descripcion:"bulterrier"},
+      {id:"003",nombre:"vela",descripcion:"bulterrier"},
+    ]})
   })
 
 module.exports=router;
